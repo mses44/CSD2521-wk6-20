4193104433a90ef93f837b7e299bf5b71bdec166
mses44 <51460821+mses44@users.noreply.github.com>
2019-09-20 00:55:57 -0400

    Update HelloTest1.java
    I added an update after line 2 and changed the comments on line 8.

--- HelloTest1.java
+++ HelloTest1.java
@@ -1,11 +1,12 @@
 /*
  * Created on 8/3/18 by AJT
+   Updated on 9/20/2019 by XYZ
  */
 package hello.test.pkg1;
 
 public class HelloTest1 {
     /*
-     * Replace your comments here!!
+     * This is my first Pull Request !!
      */
     public static void main(String[] args) {
         System.out.println("Welcome");
